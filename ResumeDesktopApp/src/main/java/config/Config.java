7ec3ae98b139d05904c46c36971c715fe53b379c
Anar-Mammadzada <anarmammadza@gmail.com>
2022-03-24 13:38:47 +0400

    ResumeDesktopProject

---
+++ ResumeDesktopApp/src/main/java/config/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import entity.User;
+
+/**
+ *
+ * @author Anar
+ */
+public class Config {
+    public static User loggedInUser;
+}
